8ee43696022f52ebaa71cecb01178d6a9bf89776
Jim Drey <moof@moofit.com>
2014-12-16 15:22:50 -0500

    Use jQuery instead of an odd mix

--- whatcolor.js
+++ whatcolor.js
@@ -1,14 +1,16 @@
+var $body = $('body');
+var $clock = $('#clock');
+var $color = $('#color');
+
 function dotime() {
-    $("body").css({"transition": "all 0.8s", "-webkit-transition": "all 0.8s"});
-    
     var d = new Date();
     var hours = d.getHours();
     var mins = d.getMinutes();
     var secs = d.getSeconds();
     
-    if (hours < 10){hours = "0" + hours};
-    if (mins < 10){mins = "0" + mins};
-    if (secs < 10){secs = "0" + secs};
+    if (hours < 10) {hours = "0" + hours};
+    if (mins < 10)  {mins = "0" + mins};
+    if (secs < 10)  {secs = "0" + secs};
     
     hours.toString();
     mins.toString();
@@ -16,8 +18,8 @@ function dotime() {
     
     var hex = "#" + hours + mins + secs;
     
-    $("#t").html(hours +" : "+ mins +" : "+ secs);
-    $("#h").html(hex);
+    $clock.text(hours + " : " + mins + " : " + secs);
+    $color.text(hex);
     
-    document.body.style.background = hex;
+    $body.css('background-color', hex);
 }
